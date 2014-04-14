4f74dabddb8a93b2b4d1d961bd4ec482447274ea
Anthony Clark <clark.anthony.g@gmail.com>
2014-04-14 16:54:08 -0400

    Initial Commit

---
+++ interpreter.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+# Anthony Clark
+#	Simple C/GCC evaluation loop
+#
+
+import os
+import sys
+import readline
+
+def main(argc, argv):
+	pass
+
+if __name__ == "__main__":
+	try:
+		main(len(sys.argv), sys.argv)
+	except KeyboardInterrupt as e:
+		pass
